5bd92f0132d2095e01fc8e5bd7735a253019fb43
VIGNESHKM15 <69468034+VIGNESHKM15@users.noreply.github.com>
2024-04-09 19:52:30 +0530

    c

--- components/dashboard/script.js
+++ components/dashboard/script.js
@@ -1,8 +1,5 @@
-const hamMenu = document.querySelector(".ham-menu");
-
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
+document.querySelectorAll('.parent-link').forEach(link => {
+  link.addEventListener('click', () => {
+      link.parentElement.classList.toggle('active'); // Toggle 'active' class on parent element
+  });
 });
